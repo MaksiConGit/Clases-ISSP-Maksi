28f527d30e40a71b0ff2ebdf71e314521c524bc8
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-10-28 20:39:37 -0300

    feat<profesor>: validaciones de edit profesor

--- Universidad/Controllers/editarProfesor.php
+++ Universidad/Controllers/editarProfesor.php
@@ -2,27 +2,43 @@
 
 require_once __DIR__ .'/../Model/Materia.php';
 require_once __DIR__ .'/../Model/Profesor.php';
+require_once __DIR__ .'/../Requests/Requests.php';
 
 $id = $_GET['id'];
+$profesor = Profesor::getById($id);
+
+$nombre = $profesor->nombre;
+$apellido = $profesor->apellido;
+$materia_id = $profesor->materia_id;
+
+$materias = Materia::all();
+
+$errores = [];
 
 if(isset($_POST['actualizarDatos'])){
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $materia_id = $_POST['materia_id'];
 
-    $profesor = Profesor::getById($id);
-    $profesor->nombre = $nombre;
-    $profesor->materia_id = $materia_id;
-    $profesor->update();
+    $errores = array_merge(
+        camposVacios([$nombre, $apellido, $materia_id]),
+        maxMin(['nombre' => $nombre, 'apellido' => $apellido]),
+        soloLetras(['nombre' => $nombre, 'apellido' => $apellido]),
+    );
 
-    header('Location: ../indexProfesor.php');
-} else  {
-    $profesor = Profesor::getById($id);
-    $materias = Materia::all();
-    
-    if ($profesor) {
-        require_once __DIR__ .'/../Views/profesores/editarProfesor.view.php';
+    if (empty($errores)) {
+        $profesor = Profesor::getById($id);
+        $profesor->nombre = $nombre;
+        $profesor->materia_id = $materia_id;
+        $profesor->update();
+
+        header('Location: ../indexProfesor.php');
     }
 }
+
+if ($profesor) {
+    require_once __DIR__ .'/../Views/profesores/editarProfesor.view.php';
+}
+
 
 
