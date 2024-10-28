55dd590bea1f6e704b3070a458eae8c8be0e4038
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-10-28 15:36:58 -0300

    feat<alumno>: validaciones en edit alumno

--- Universidad/Views/alumnos/editarAlumno.view.php
+++ Universidad/Views/alumnos/editarAlumno.view.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Alumno</title>
     <!-- Include bootstrap last version -->
@@ -11,6 +11,8 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- Include fontawesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
 </head>
 
@@ -29,15 +31,15 @@
                         <form action="" method="post">
                             <div class="form-group">
                                 <label for="nombre">Nombre</label>
-                                <input type="text" value="<?= $alumno->nombre ?>" name="nombre" id="nombre" class="form-control">
+                                <input type="text" value="<?= $nombre ?>" name="nombre" id="nombre" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="apellido">Apellido</label>
-                                <input type="text" value="<?= $alumno->apellido ?>" name="apellido" id="apellido" class="form-control">
+                                <input type="text" value="<?= $apellido ?>" name="apellido" id="apellido" class="form-control">
                             </div>
                             <div class="form-group" id="fecha_nacimiento">
                                 <label for="fecha_nacimiento">Fecha de Nacimiento</label>
-                                <input type="date" value="<?= $alumno->fecha_nacimiento ?>" name="fecha_nacimiento" id="fecha_nacimiento" class="form-control">
+                                <input type="date" value="<?= $fecha_nacimiento ?>" name="fecha_nacimiento" id="fecha_nacimiento" class="form-control">
                             </div>
                             <div class="form-group" id="">
 
@@ -59,10 +61,39 @@
 
                             <?php } ?>
 
-                            <br>
-                            <button type="submit" name="actualizarDatos" class="btn btn-primary">
-                                <i class="fas fa fa-send">Actualizar</i>
+                                                       <!-- Button trigger modal -->
+                                                       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Enviar
                             </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">¿Quiere cargar este alumno?</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Los datos podrán ser modificados más adelante.
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Descartar</button>
+                                    <button type="submit" class="btn btn-primary" name="actualizarDatos" class="btn btn-primary" >Guardar cambios</button>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <?php
+
+                            foreach ($errores as $error) { ?>
+
+                            <div class="alert alert-danger p-1 mt-3" role="alert">
+                                <?= $error ?>
+                            </div>
+                                
+                            <?php } ?>
 
                         </form>
 
