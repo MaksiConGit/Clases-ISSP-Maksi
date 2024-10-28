b5c985587d3b984c44662ad539d262265bfbbc2b
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-10-28 14:54:22 -0300

    feat<alumno>: validaciones
    Se crea un nuevo archivo de validaciones con funciones para validar datos. Se comprueba si existen errores y los muestra si es así, y si no, guarda el alumno.

---
+++ Universidad/Requests/Requests.php
@@ -0,0 +1,69 @@
+<?php
+
+    function camposVacios($campos){
+
+        $errores = [];
+
+        foreach ($campos as $campo) {
+            if (empty($campo)) {
+                $errores[] = "- Todos los campos son obligatorios.";
+                return $errores;
+            }
+        }
+
+        return $errores;
+    }
+    
+    function maxMin($campos){
+
+        $errores = [];
+
+        foreach ($campos as $nombre_campo => $valor_campo) {
+            if (strlen($valor_campo) <= 1) {
+                $errores[] = "- El campo $nombre_campo debe tener mínimo 2 caracteres.";
+            }
+            elseif (strlen($valor_campo) >= 60 ) {
+                $errores[] = "- El campo $nombre_campo debe tener máximo 60 caracteres.";
+            }
+        }
+
+        return $errores;
+    }
+
+    function soloLetras($campos){
+
+        $errores = [];
+
+        foreach ($campos as $nombre_campo => $valor_campo) {
+
+            foreach (str_split($valor_campo) as $letra) {
+
+                if (!ctype_alpha($letra)) {
+                    $errores[] = "- El campo $nombre_campo debe estar conformado por letras.";
+                    break;
+                }
+            }
+                
+        }
+
+        return $errores;
+    
+    }
+
+    function fechaAntesDeHoy($campos){
+
+        $errores = [];
+
+        foreach ($campos as $campo) {
+            $fecha_nacimiento_formateada = date('Y-m-d', strtotime($campo));
+            $fecha_actual = date('Y-m-d');
+
+            if ($fecha_nacimiento_formateada >= $fecha_actual) {
+                $errores[] = "- La fecha no puede ser hoy o después de hoy.";
+            }
+
+            return $errores;
+
+        }
+
+    }
